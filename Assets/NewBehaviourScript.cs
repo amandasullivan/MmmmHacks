6cc4168ab6dd9697cf76bb00e0aacdf84c893c22
argentco <argentco@umich.edu>
2015-09-12 19:34:30 -0400

    Importing the Unity Files

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+        speed = 2f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(speed*Input.GetAxis("Horizontal")*Time.deltaTime,0f,speed*Input.GetAxis("Vertical")*Time.deltaTime);
+    }
+}
